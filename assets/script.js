d6eb3695eb3a8a107cb5516e6b8a0c2dfe9cd332
Axh <anikahussain1@outlook.com>
2023-04-24 15:51:37 +0100

    window alerts added for password length

--- assets/script.js
+++ assets/script.js
@@ -23,13 +23,25 @@ var passwordLowCased = false;
 var passwordUppCased = false;
 
 //first window promt (number of characters)
-length = window.prompt("How many CHARACTERS do you want your password to contain?");
+pLength = window.prompt("How many CHARACTERS do you want your password to contain?");
 
-//second window promt (if password is out of password limits)
-if (length < 8 && length > 128) {
+//second window alert (if password is out of password limits)
+if (pLength < 8 && pLength > 128) {
   window.alert("Password MUST be between 8-128 characters, please try again");
-  return;
+  return null;
 }
+//window alert (if password if less that 8 characters)
+if (pLength < 8) {
+  window.alert("Password MUST have a minimum length of 8 characters or more, please try again");
+  return null;
+}
+//window alert (if password is more than 128 characters)
+if (pLength > 128) {
+  window.alert ("Password MUST have a maximum length of 128 characters or less, please try again");
+  return null;
+}
+
+
 
 
 }
