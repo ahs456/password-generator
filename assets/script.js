db4e5fec8352de36a6ef574aeacb6b18acdceaf7
Axh <anikahussain1@outlook.com>
2023-04-24 15:17:31 +0100

    initialised password criteria, added window popup

--- assets/script.js
+++ assets/script.js
@@ -9,13 +9,28 @@ function writePassword() {
   passwordText.value = password;
 }
 
+//Generating password criteria
 function generatePassword() {
-  const lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-  const upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerCasedCharacters = "abcdefghijklmnopqrstuvwxyz";
+  const upperCasedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numbers = "0123456789";
   const special = "!@£$%^&*()'+,-./:;<=>?[]\_`{}|~#";
 
+//initialising password criteria to baseline
+var length = 0;
+var passwordNumbers = false;
+var passwordSpecial = false;
+var passwordLowCased = false;
+var passwordUppCased = false;
+
+//first window promt
+length = window.prompt("How many CHARACTERS do you want your password to contain?");
+
+
+
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
